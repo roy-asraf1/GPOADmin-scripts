e6adc25e0c6aca067bf20bf325133af56731793e
Roy Asraf <117677549+roy-asraf1@users.noreply.github.com>
2025-07-30 18:55:57 +0300

    Update serviceAccounts.ps1

--- serviceAccounts.ps1
+++ serviceAccounts.ps1
@@ -3,3 +3,5 @@ Get-WmiObject -Class Win32_Service | Select-Object Name, StartName | Sort-Object
 Get-Service | Where-Object { $_.DisplayName -like "*GPO*" } | ForEach-Object {
     Get-WmiObject -Class Win32_Service -Filter "Name='$($_.Name)'" | Select-Object Name, StartName
 }
+
+Start-Transcript -Path "C:\Logs\ScriptLog.txt" -Append
